9dcb584a8133d0e7d4b5385862c7868febc2137d
Jarrod <jarrod83miller@gmail.com>
2022-11-27 17:48:52 -0500

    added review stars to ratings component

---
+++ frontend/src/components/Rating.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+export const Rating = ({ value, number }) => {
+  let fullStar = "fa-solid fa-star";
+  let halfStar = "fa-solid fa-star-half-stroke";
+  let emptyStar = "fa-regular fa-star";
+  return (
+    <div className="rating">
+      <span>
+        <i
+          className={
+            value >= 1
+              ? "fa-solid fa-star"
+              : value >= 0.5
+              ? "fa-solid fa-star-half-stroke"
+              : "fa-regular fa-star"
+          }
+        />
+        <i
+          className={
+            value >= 2
+              ? "fa-solid fa-star"
+              : value >= 1.5
+              ? "fa-solid fa-star-half-stroke"
+              : "fa-regular fa-star"
+          }
+        />
+        <i
+          className={
+            value >= 3
+              ? "fa-solid fa-star"
+              : value >= 2.5
+              ? "fa-solid fa-star-half-stroke"
+              : "fa-regular fa-star"
+          }
+        />
+        <i
+          className={
+            value >= 4
+              ? "fa-solid fa-star"
+              : value >= 3.5
+              ? "fa-solid fa-star-half-stroke"
+              : "fa-regular fa-star"
+          }
+        />
+        <i
+          className={
+            value >= 5
+              ? "fa-solid fa-star"
+              : value >= 4.5
+              ? "fa-solid fa-star-half-stroke"
+              : "fa-regular fa-star"
+          }
+        />
+      </span>
+    </div>
+  );
+};
