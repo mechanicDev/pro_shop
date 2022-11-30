9b5770d11c216a15862c62a323f9dbc0b85762c6
Jarrod <jarrod83miller@gmail.com>
2022-11-29 19:21:35 -0500

    Added Link containers to header/navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,17 +7,23 @@ const Header = () => {
     <header>
       <Navbar bg='light' expand='lg'>
         <Container>
-          <Navbar.Brand href='/'>Pro Shop</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand href='/'>Pro Shop</Navbar.Brand>
+          </LinkContainer>
 
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link href='/cart'>
-                <i className='fas fa-shopping-cart'></i>Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <i className='fas fa-user'></i>Sign In
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link href='/cart'>
+                  <i className='fas fa-shopping-cart'></i>Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i>Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
