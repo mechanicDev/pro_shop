a112c69e4ec5abe94dcccdfe1509a92409f81529
Jarrod <jarrod83miller@gmail.com>
2022-12-12 17:11:29 -0500

    moved env to root

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,21 @@
 const express = require("express");
 const app = express();
-PORT = process.env.PORT || 5000;
+const DOTENV = require("dotenv");
+DOTENV.config();
+const PORT = process.env.PORT || 5000;
+const NODE_ENV = process.env.NODE_ENV;
 const products = require("./data/products");
 
-require("dotenv").config();
-
 app.get("/api/products", (req, res) => {
-  console.log("TEsting");
   res.status(200).send(products);
 });
 
 app.get("/api/products/:id", (req, res) => {
-  console.log("Running??");
   let id = req.params.id;
   let product = products.find((p) => id === p._id);
   res.status(200).json(product);
 });
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+  console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}:`);
 });
